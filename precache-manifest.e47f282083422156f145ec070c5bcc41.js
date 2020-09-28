4a2e7fd17e855393e0663f602f42f30dbc11b9e0
Maria Balukleeva <mary.balkleeva10@gmail.com>
2020-09-28 17:22:46 +0300

    Updates

---
+++ precache-manifest.e47f282083422156f145ec070c5bcc41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e562c2cfcdb30e49c7c7ec4d51249521",
+    "url": "/frontend-2/index.html"
+  },
+  {
+    "revision": "83d6caf705f5adcb4c46",
+    "url": "/frontend-2/static/css/main.e274087a.chunk.css"
+  },
+  {
+    "revision": "7512d5fb0acccfeab176",
+    "url": "/frontend-2/static/js/2.f0d0a8e9.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/frontend-2/static/js/2.f0d0a8e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d6caf705f5adcb4c46",
+    "url": "/frontend-2/static/js/main.e08498b1.chunk.js"
+  },
+  {
+    "revision": "7f986307462d34c391f4",
+    "url": "/frontend-2/static/js/runtime-main.61e3922e.js"
+  }
+]);
